21c50a00b29540b1fbaa12a81c24060ca53731a7
OM SONI <166503419+mrOmsoni@users.noreply.github.com>
2026-06-09 18:14:15 +0530

    Configure CORS with dynamic client URL

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ connectDB()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
