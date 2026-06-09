9cec0d1f0b569e3597fffa17872a4499c3449d9d
OM SONI <166503419+mrOmsoni@users.noreply.github.com>
2026-06-09 18:30:13 +0530

    Allow fallback origin for CORS configuration

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB()
 const app = express()
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: process.env.CLIENT_URL || '*',
   credentials: true,
 }))
 app.use(express.json())
